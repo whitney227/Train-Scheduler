009181d690bab29dbad2a33bc8c9f96fef870c4f
Whitney Crawford <whitneylcrawford@gmail.com>
2019-04-07 20:59:21 -0400

    working app

--- assets/train.js
+++ assets/train.js
@@ -1,5 +1,4 @@
- console.log("ready");
- // Initialize Firebase
+// Initialize Firebase
  var config = {
     apiKey: "AIzaSyDoPwCwAr_TF-uuELSdx6VMyUtoS_k-yTg",
     authDomain: "my-train-project-e5f01.firebaseapp.com",
@@ -10,17 +9,13 @@
 };
 firebase.initializeApp(config);
 
-//Declare Variables
-var tFrequency = "";
-var firstTime = "";
-var trainName= "";
-var destination="";
+var dataRef=firebase.database();
 
 //Listener for the submit button
 $("#submit").on('click', function(event){
     //this method prevents the page from being reloaded
     event.preventDefault();
-
+    
     //capture the input and store it into variables
     trainName = $('#inputTrainName').val().trim();
     destination = $('#inputDestination').val().trim();
@@ -28,44 +23,69 @@ $("#submit").on('click', function(event){
     tFrequency = $('#tFrequency').val().trim();
 
     //console log to confirm that input is being received
-    console.log(trainName);
-    console.log(destination);
-    console.log(firstTime);
-    console.log(tFrequency);
+    console.log("trainName");
+    console.log("destination");
+    console.log("firstTime");
+    console.log("tFrequency");
 
-    //Push data to Firebase as a new child
-    firebase.database().ref().set({
+    //Creat a local "temporary" object for holding data
+    var newTrain = {
         trainName: trainName,
         destination: destination,
         firstTime: firstTime,
         tFrequency: tFrequency,
-    })
+        dataAdded: firebase.database.ServerValue.TIMESTAMP
+    };
+     //Push data to Firebase as a new child
+     dataRef.ref().push(newTrain);
+
+     //Reset user input form
+     document.forms[0].reset();
 })
 
-//add variables to calculate when the next train will arrive
+//Create a Firebase watcher for when a user adds an entry
+dataRef.ref().on("child_added", function(childSnapshot){
+    console.log(childSnapshot.val());
+    //store returned data into variables
+    var name = childSnapshot.val().trainName;
+    var dest= childSnapshot.val().destination;
+    var time = childSnapshot.val().firstTime;
+    var frequency = childSnapshot.val().tFrequency;
 
-// First Time (pushed back 1 year to make sure it comes before current time)
-var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-console.log(firstTimeConverted);
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(time, "hh:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
 
-// Current Time
-var currentTime = moment();
-console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
-// Difference between the times
-var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-console.log("DIFFERENCE IN TIME: " + diffTime);
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
 
-// Time apart (remainder)
-var tRemainder = diffTime % tFrequency;
-console.log(tRemainder);
+    // Time apart (remainder)
+    var tRemainder = diffTime % frequency;
+    console.log(tRemainder);
 
-// Minute Until Train
-var tMinutesTillTrain = tFrequency - tRemainder;
-console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+    // Minute Until Train
+    var tMinutesTillTrain = frequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
-// Next Train
-var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+    //Create a new row for HTML table
+    var newRow = $("<tr>").append(
+        $("<td>").text(name),
+        $("<td>").text(dest),
+        $("<td>").text(frequency),
+        $("<td>").text(nextTrain),
+        $("<td>").text(tMinutesTillTrain)
+    );
+    //Append new row
+    $("#trainSchedule > tbody").append(newRow);
+});
 
 
